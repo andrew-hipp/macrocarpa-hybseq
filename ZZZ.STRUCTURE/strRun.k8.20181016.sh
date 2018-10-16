2c8d6bbf023f8a4c77a422e7f13049063274d9de
Andrew Hipp <andrew.hipp@gmail.com>
2018-10-16 12:20:00 -0500

    Adding structure runs for k8, k9

---
+++ ZZZ.STRUCTURE/strRun.k8.20181016.sh
@@ -0,0 +1,1 @@
+console/structure -m params.shortRun -i hybseq.v3.2018-10-10.str -L 12714 -N 263 -K 8 -o hybseq.k8.20181016
---
+++ ZZZ.STRUCTURE/strRun.k9.20181016.sh
@@ -0,0 +1,1 @@
+console/structure -m params.shortRun -i hybseq.v3.2018-10-10.str -L 12714 -N 263 -K 9 -o hybseq.k9.20181016
