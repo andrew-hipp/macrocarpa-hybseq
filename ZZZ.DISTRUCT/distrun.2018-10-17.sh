95c7cedbfac05fa233229bd2026d25a4120cff96
Andrew Hipp <andrew.hipp@gmail.com>
2018-10-17 08:29:55 -0500

    Adding DISTRUCT output

---
+++ ZZZ.DISTRUCT/distrun.2018-10-17.sh
@@ -0,0 +1,4 @@
+./distruct -M 5 -N 263 -K 9 -b quercus.pops -c quercus.perm -p hybseq.k9.20181016_f.popq -i hybseq.k9.20181016_f.indivq -o hybseq.k9.20181016_f.ps -d drawparams
+./distruct -M 5 -N 263 -K 8 -b quercus.pops -c quercus.perm -p hybseq.k8.20181016_f.popq -i hybseq.k8.20181016_f.indivq -o hybseq.k8.20181016_f.ps -d drawparams
+./distruct -M 5 -N 263 -K 10 -b quercus.pops -c quercus.perm -p hybseq.k10.20181016_f.popq -i hybseq.k10.20181016_f.indivq -o hybseq.k10.20181016_f.ps -d drawparams
+
