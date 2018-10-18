fc41852541385df0d9f47c55f1ba5719c15c9121
Andrew Hipp <andrew.hipp@gmail.com>
2018-10-18 09:34:47 -0500

    tree ladderized, rooted

--- SCRIPTS/03.tree.R
+++ SCRIPTS/03.tree.R
@@ -1,4 +1,8 @@
 library(ape)
 library(phytools)
+library(magrittr)
 
-tr <- read.tree('../DATA/trees/RAX.2018-10-15/DATA/trees/RAX.2018-10-15/RAxML_bipartitions.macrocarpa.hybseq.2018-10-15.tre')
+tr <- read.tree('../DATA/trees/RAX.2018-10-15/RAxML_bipartitions.macrocarpa.hybseq.2018-10-15.tre')
+tr <- root(tr, grep('rubra', tr$tip.label)) %>%
+      ladderize %>%
+      drop.tip(tip = grep('rubra', tr$tip.label))
