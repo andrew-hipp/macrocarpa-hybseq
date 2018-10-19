fafcccffffd306920379bc066aa47e848a694d4f
md-garner <mgarner@mortonarb.org>
2018-10-19 09:05:49 -0500

    output of 00 and 01 R scripts

---
+++ working/distruct.hybseq.v3.2018-10-10.sh
@@ -0,0 +1,1 @@
+./distruct -M 5 -N 263 -K 5 -b hybseq.v3.2018-10-10.pop_labels -c quercus.perm -p hybseq.v3.2018-10-10_f.popq -i hybseq.v3.2018-10-10_f.indvq -o hybseq.v3.2018-10-10.ps -d drawparams
